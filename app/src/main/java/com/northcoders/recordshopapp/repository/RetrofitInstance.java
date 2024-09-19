f85402c6c241a74322070435cafc5acc2c3ed2a5
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-19 11:56:22 +0100

    feat: implement logic for RetrofitInstance class

--- app/src/main/java/com/northcoders/recordshopapp/repository/RetrofitInstance.java
+++ app/src/main/java/com/northcoders/recordshopapp/repository/RetrofitInstance.java
@@ -1,4 +1,22 @@
 package com.northcoders.recordshopapp.repository;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class RetrofitInstance {
+
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "";
+
+    public static RecordShopApiService getService() {
+        if(retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+
+        return retrofit.create(RecordShopApiService.class);
+    }
+
 }
