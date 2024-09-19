b903338a3e4cbb394cb165c1c1ecd751dcec6c41
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-19 14:55:19 +0100

    add GET call function for RecordShopApiService interface

--- app/src/main/java/com/northcoders/recordshopapp/repository/RecordShopApiService.java
+++ app/src/main/java/com/northcoders/recordshopapp/repository/RecordShopApiService.java
@@ -1,4 +1,14 @@
 package com.northcoders.recordshopapp.repository;
 
+import com.northcoders.recordshopapp.model.Record;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RecordShopApiService {
+
+    @GET
+    Call<List<Record>> getAllRecords();
 }
