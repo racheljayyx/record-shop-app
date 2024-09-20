c1355f8404342d92b424b24a20122867b6d6259a
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-20 14:00:10 +0100

    feat:implement logic for MainActivityViewModel class

--- app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/MainActivityViewModel.java
+++ app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/MainActivityViewModel.java
@@ -1,4 +1,30 @@
 package com.northcoders.recordshopapp.ui.mainactivity;
 
-public class MainActivityViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+
+import com.northcoders.recordshopapp.model.Record;
+import com.northcoders.recordshopapp.repository.RecordShopRepository;
+
+import java.util.List;
+
+public class MainActivityViewModel extends AndroidViewModel {
+
+    private final RecordShopRepository recordShopRepository;
+
+    public MainActivityViewModel(@NonNull Application application) {
+        super(application);
+        this.recordShopRepository = new RecordShopRepository();
+    }
+
+    public LiveData<List<Record>> getAllRecords() {
+        return recordShopRepository.getMutableLiveData();
+    }
+
+
+
 }
