228beadf7cf3096a90adb4ffe4940f98c4a745f8
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-23 15:23:07 +0100

    feat: add POST request

--- app/src/main/java/com/northcoders/recordshopapp/repository/RecordShopApiService.java
+++ app/src/main/java/com/northcoders/recordshopapp/repository/RecordShopApiService.java
@@ -5,10 +5,15 @@ import com.northcoders.recordshopapp.model.Record;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface RecordShopApiService {
 
     @GET
     Call<List<Record>> getAllRecords();
+
+    @POST
+    Call<Record> addRecord(@Body Record record);
 }
