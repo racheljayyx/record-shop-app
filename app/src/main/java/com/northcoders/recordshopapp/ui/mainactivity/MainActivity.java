65f055e46672f83533c550d0ef75c3105d686015
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-23 15:22:44 +0100

    feat: implement logic to be able to call from the backend record shop API and display in the application

--- app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/MainActivity.java
+++ app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.northcoders.recordshopapp.ui.mainactivity;
 
+import static java.lang.reflect.Array.get;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,13 +10,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.northcoders.recordshopapp.R;
+import com.northcoders.recordshopapp.databinding.ActivityMainBinding;
+import com.northcoders.recordshopapp.model.Record;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private ArrayList<Record> records;
+    private RecordShopAdapter recordShopAdapter;
+    private MainActivityViewModel mainActivityViewModel;
+    private ActivityMainBinding activityMainBinding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainActivityViewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+        getAllRecords();
+
+
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
@@ -23,4 +47,25 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    private void getAllRecords() {
+        mainActivityViewModel.getAllRecords().observe(this, new Observer<List<Record>>() {
+            @Override
+            public void onChanged(List<Record> recordsFromLiveData) {
+                records = (ArrayList<Record>) recordsFromLiveData;
+                displayInRecyclerView();
+            }
+        });
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void displayInRecyclerView() {
+        recyclerView = activityMainBinding.recyclerView;
+        recordShopAdapter = new RecordShopAdapter(records, this);
+        recyclerView.setAdapter(recordShopAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+        recordShopAdapter.notifyDataSetChanged();
+    }
+
 }
--- app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/RecordShopAdapter.java
+++ app/src/main/java/com/northcoders/recordshopapp/ui/mainactivity/RecordShopAdapter.java
@@ -1,4 +1,54 @@
 package com.northcoders.recordshopapp.ui.mainactivity;
 
-public class RecordShopAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.northcoders.recordshopapp.databinding.RecordItemBinding;
+import com.northcoders.recordshopapp.model.Record;
+
+import java.util.List;
+
+public class RecordShopAdapter extends RecyclerView.Adapter<RecordShopAdapter.RecordShopViewHolder> {
+
+    private List<Record> records;
+    private Context context;
+
+    public RecordShopAdapter(List<Record> records, Context context) {
+        this.records = records;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public RecordShopViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecordShopViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+
+    public static class RecordShopViewHolder extends RecyclerView.ViewHolder {
+
+        private final RecordItemBinding recordItemBinding;
+
+        public RecordShopViewHolder(RecordItemBinding recordItemBinding) {
+            super(recordItemBinding.getRoot());
+            this.recordItemBinding = recordItemBinding;
+        }
+
+
+    }
+
 }
